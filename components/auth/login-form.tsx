cab26bb161f61c973dc67c3891dcda422597f100
gaurimadan <gaurimadan12@gmail.com>
2024-09-05 02:41:46 +0530

    server added

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -7,6 +7,10 @@ import { LoginSchema } from "@/schemas";
 import * as z from "zod";
 import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
 import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+import { login } from "@/actions/login";
 
 export const LoginForm = () => {
     const form = useForm<z.infer<typeof LoginSchema>>({
@@ -17,6 +21,10 @@ export const LoginForm = () => {
         },
     });
 
+    const onSubmit=(values:z.infer<typeof LoginSchema>)=>{
+        login(values);
+    }
+
     return (
         <CardWrapper
             headerLabel="Welcome Back"
@@ -26,7 +34,7 @@ export const LoginForm = () => {
             
             {/* Use FormProvider to provide the form context */}
             <FormProvider {...form}>
-                <form onSubmit={form.handleSubmit(() => {})} className="space-y-6">
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
                     <div className="space-y-4">
                         <FormField
                             control={form.control}
@@ -59,6 +67,13 @@ export const LoginForm = () => {
                                 </FormItem>
                             )} />
                     </div>
+                    <FormError message=""/>
+                    <FormSuccess message=""/>
+                    <Button
+                    type="submit"
+                    className="w-full">
+                        Login
+                    </Button>
                 </form>
             </FormProvider>
         </CardWrapper>
