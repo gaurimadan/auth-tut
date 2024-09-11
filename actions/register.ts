e2d3891bfe30dc607012a20ea01489576b6e6261
gaurimadan <gaurimadan12@gmail.com>
2024-09-11 19:23:56 +0530

    data added

--- actions/register.ts
+++ actions/register.ts
@@ -4,6 +4,7 @@ import { RegisterSchema } from "@/schemas";
 import bcrypt from "bcrypt";
 import { db } from "@/lib/db";
 import { error } from "console";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values:z.infer<typeof RegisterSchema>) =>{
     const validatedFields = RegisterSchema.safeParse(values);
@@ -14,11 +15,7 @@ export const register = async (values:z.infer<typeof RegisterSchema>) =>{
     const {email,password,name} = validatedFields.data;
     const hashPassword = await bcrypt.hash(password,10);
 
-    const existingUser = await db.user.findUnique({
-        where:{
-            email,
-        }
-    })
+    const existingUser = await getUserByEmail(email);
 
     if(existingUser){
         return {error:"Email already taken!"};
