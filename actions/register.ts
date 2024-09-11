eac668aae4a917dbb0ad2c741353133d5c30ac9b
gaurimadan <gaurimadan12@gmail.com>
2024-09-11 18:31:16 +0530

    database linked

--- actions/register.ts
+++ actions/register.ts
@@ -1,11 +1,35 @@
 "use server";
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { error } from "console";
 
 export const register = async (values:z.infer<typeof RegisterSchema>) =>{
     const validatedFields = RegisterSchema.safeParse(values);
     if(!validatedFields.success){
         return {error:"Invalid Fields!" };
     }
-    return {success:"Email verified! "};
+
+    const {email,password,name} = validatedFields.data;
+    const hashPassword = await bcrypt.hash(password,10);
+
+    const existingUser = await db.user.findUnique({
+        where:{
+            email,
+        }
+    })
+
+    if(existingUser){
+        return {error:"Email already taken!"};
+    }
+
+    await db.user.create({
+        data:{
+            name,
+            email,
+            password:hashPassword,
+        }
+    })
+    return {success:"User created "};
 }
