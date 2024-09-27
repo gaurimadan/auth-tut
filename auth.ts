a915840f59ca0c608f8b3b5186d3c1ed0d16e8f9
gaurimadan <gaurimadan12@gmail.com>
2024-09-28 02:20:35 +0530

    github and google added

--- auth.ts
+++ auth.ts
@@ -1,15 +1,46 @@
-import NextAuth from "next-auth"
+import NextAuth,{DefaultSession} from "next-auth";
+ 
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 import authConfig from "./auth.config"
 import { db } from "./lib/db"
- 
+import { getUserById } from "./data/user"
+import {UserRole} from "@prisma/client";
+
+
+
 const prisma = new PrismaClient()
  
 export const { auth, handlers, signIn, signOut } = NextAuth({
   callbacks:{
+    async signIn({user}) {
+      const existingUser = await getUserById(user.id);
+      // if(!existingUser ||!existingUser.emailVerified){
+      //   return false;
+      // }
+      return true;
+    },
+    async session({token,session}) {
+      console.log({
+        sessioToken:token,
+      })
+      if(token.sub && session.user){
+        session.user.id = token.sub;
+      }
+      if(token.role && session.user){
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    
+    },
     async jwt({token}) {
-      console.log({token})
+
+      if(!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if(!existingUser) return token;
+
+      token.role = existingUser.role;
       return token;
     }
   },
