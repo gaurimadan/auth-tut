3a21b346acf2d48822a76f447aa050284b06ee52
gaurimadan <gaurimadan12@gmail.com>
2024-11-01 19:10:40 +0530

    settings page modified

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,19 +1,24 @@
-import { auth, signOut } from "@/auth";
+"use client";
+
+import { logout } from "@/actions/logout";
+import { useCurrentUser } from "@/hooks/use-current-user";
 
 
-const SettingPage = async () =>{
-    const session = await auth();
+const SettingPage =  () =>{
+    const user = useCurrentUser();
+   
+    const onClick= () => {
+        logout();
+    };
+    
     return(
         <div>
-           {JSON.stringify(session)}
-           <form action={async()=>{
-            "use server";
-            await signOut();
-           }}>
-            <button type="submit">
+           {JSON.stringify(user)}
+           
+            <button  onClick={onClick} type="submit">
                 Sign Out
             </button>
-           </form>
+           
         </div>
     )
 }
--- components/auth/user-button.tsx
+++ components/auth/user-button.tsx
@@ -4,9 +4,13 @@ import { Avatar } from "@radix-ui/react-avatar";
 import { DropdownMenu, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
 import { AvatarFallback, AvatarImage } from "../ui/avatar";
 import { FaUser } from "react-icons/fa";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { DropdownMenuContent } from "../ui/dropdown-menu";
+import { LogoutButton } from "./logout-button";
+import { ExitIcon } from "@radix-ui/react-icons";
 
 export const UserButton = () => {
-
+        const user = useCurrentUser();
             return (
                <DropdownMenu>
                 <DropdownMenuTrigger>
@@ -17,6 +21,12 @@ export const UserButton = () => {
                         </AvatarFallback>
                     </Avatar>
                 </DropdownMenuTrigger>
+                <DropdownMenuContent className="w-40" align="end">
+                    <LogoutButton>
+                        <ExitIcon className="h-4 w-4 mr-2"/>
+                        Logout
+                    </LogoutButton>
+                </DropdownMenuContent>
                </DropdownMenu>
             )
 }
